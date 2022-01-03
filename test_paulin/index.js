a470c2eaf9f97ca8900efb8b25c8b9650db99d59
PaulinZumstein <paulin.zumstein@jellyfish.com>
2022-01-03 10:56:02 +0100

    test

---
+++ test_paulin/index.js
@@ -0,0 +1,9 @@
+var M = {}, V = {}, C = {};
+
+M.data = "hello world";
+
+V.render = function (M) { alert(M.data); }
+
+C.handleOnload = function () { V.render(M); }
+
+window.onload = C.handleOnLoad;
