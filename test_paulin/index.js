536822f901225bc2f179cd5a51158f1c70cdaa80
dylan-pereira <dylan.pereira30@orange.fr>
2022-01-04 18:43:58 +0100

    clean branch init

--- test_paulin/index.js
+++
@@ -1,9 +0,0 @@
-var M = {}, V = {}, C = {};
-
-M.data = "hello world";
-
-V.render = function (M) { alert(M.data); }
-
-C.handleOnload = function () { V.render(M); }
-
-window.onload = C.handleOnLoad;
