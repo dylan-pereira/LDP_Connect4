f101a534ee8a6afaa0cf2bdac614bc7c7d8425fe
Paulin-Zumstein <paulin.zumstein@gmail.com>
2022-01-13 11:32:42 +0100

    delete spaces

--- Model/board_model.js
+++ Model/board_model.js
@@ -69,7 +69,6 @@ class Grille {
 
     check_diags(jeton) {
         var diagsRight, diagsLeft = false;
-
         for (let ligne = 0; ligne < this.hauteur; ligne++) {
             for (let colonne = 0; colonne < this.largeur; colonne++) {
                 diagsRight = this.checkDiagRight(colonne, ligne, jeton, 0) == 4;
@@ -84,25 +83,21 @@ class Grille {
     }
 
     checkDiagRight(c, l, color, val) {
-
         if(c >= 0 && c < 7 && l < 6 && val < 5 && this.tableau[c][l] == color){
             val = 1 + this.checkDiagRight(c-1, l+1, color, val);
             return val;
         } else {
             return 0;
         }
-
     }
 
     checkDiagLeft(c, l, color, val) {
-
         if (c >= 0 && l < 6 && val < 5 && this.tableau[c][l] == color) {
             val = 1 + this.checkDiagLeft(c - 1, l - 1, color, val);
             return val;
         } else {
             return 0;
         }
-
     }
 
     to_string() {
